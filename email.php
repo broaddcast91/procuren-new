cb5476553c7bbeed09eb3c893d6ab9dcce2ea977
iamharnad <haranathpriyan@gmail.com>
2023-02-10 16:13:20 +0530

    added email functionality

---
+++ email.php
@@ -0,0 +1,28 @@
+<?php 
+if(isset($_POST['submit'])){
+   $to = "haranathpriyan@gmail.com";
+    
+   
+    $name = $_POST['name']; // this is the sender's Email address
+   
+    $Email = $_POST['email'];
+     $company = $_POST['subject'];
+      $comment = $_POST['message'];
+     
+    $subject = "ProcureN Enquiry form";
+    $subject2 = "Copy of your form submission";
+    $message =  " CLIENT CONTACT DETAILS  :" . "\n\n"." Client Name : " . $_POST['name'] .  "\n\n"."E-mail :" . $_POST['email'].  "\n\n"."Company Website : " . $_POST['subject']."\n\n"."Message : " . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['email'];
+
+    $headers = "From:" .  $name;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); 
+    // sends a copy of the message to the sender
+
+echo "<script>
+    window.location = 'thank-you.php';
+</script>";
+    // to redirect to another page.
+    }
+?>
